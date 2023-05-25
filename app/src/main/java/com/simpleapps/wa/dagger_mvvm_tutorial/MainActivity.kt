79dc6c7916cb347825558fe3eaea94efa43340d9
Shubham Soni <shubhamsonicse@gmail.com>
2023-05-25 07:55:54 +0530

    first commit added

---
+++ app/src/main/java/com/simpleapps/wa/dagger_mvvm_tutorial/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.simpleapps.wa.dagger_mvvm_tutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.simpleapps.wa.dagger_mvvm_tutorial.ui.theme.DaggerMVVMTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DaggerMVVMTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DaggerMVVMTutorialTheme {
+        Greeting("Android")
+    }
+}
